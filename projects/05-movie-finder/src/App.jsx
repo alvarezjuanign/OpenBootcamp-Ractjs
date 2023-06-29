7b294805effaf9447b61bf2b39ec9ba41f0f538e
Juan Alvarez <alvarez.juanign@gmail.com>
2023-06-29 18:56:34 -0300

    Add validation at form

--- projects/05-movie-finder/src/App.jsx
+++ projects/05-movie-finder/src/App.jsx
@@ -1,18 +1,44 @@
-import './App.css'
 import { Movies } from './components/movies'
 import { useMovies } from './hooks/useMovie'
+import { useState } from 'react'
+import './App.css'
 
 export function App () {
   const { movies: mappedMovies } = useMovies()
+  const [query, setQuery] = useState()
+  const [error, setError] = useState(null)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log({ query })
+  }
+
+  const handleChange = (e) => {
+    const newQuery = e.target.value
+    setQuery(newQuery)
+
+    if (newQuery === '') {
+      setError('No se puede buscar una película vacía')
+      return
+    }
+
+    if (newQuery.length < 3) {
+      setError('Debe tener más de tres caracteres')
+      return
+    }
+
+    setError(null)
+  }
 
   return (
     <div className='page'>
       <header>
         <h1>Movie Finder</h1>
-        <form className='search-form'>
-          <input type='text' placeholder='Avengers, Star Wars, Harry Potter...' />
+        <form className='search-form' onSubmit={handleSubmit}>
+          <input onChange={handleChange} value={query} name='query' type='text' placeholder='Avengers, Star Wars, Harry Potter...' />
           <button>Search</button>
         </form>
+        {error && <p className='error'>{error}</p>}
       </header>
 
       <main>
